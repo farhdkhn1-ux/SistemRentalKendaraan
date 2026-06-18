3241579841d346d06f2b6e84ed3810b0f113d372
a00984726-blip <a00984726@gmail.com>
2026-06-18 17:10:15 +0700

    user controller

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -6,15 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
-    //
-}<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Vehicle extends Model
-{
     protected $fillable = [
         'plate_number',
         'brand',
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
 
 Route::view('/', 'welcome');
@@ -17,7 +18,16 @@ require __DIR__.'/auth.php';
 use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\RentalController;
 
-Route::middleware(['auth'])->group(function () {
+// Routes khusus Admin
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::resource('vehicles', VehicleController::class);
     Route::resource('rentals', RentalController::class);
+});
+
+// Routes khusus Customer
+Route::middleware(['auth', 'role:customer'])->prefix('customer')->name('customer.')->group(function () {
+    Route::get('/vehicles', [CustomerController::class, 'vehicles'])->name('vehicles');
+    Route::get('/booking/{vehicle}', [CustomerController::class, 'bookingForm'])->name('booking');
+    Route::post('/booking/{vehicle}', [CustomerController::class, 'bookingStore'])->name('booking.store');
+    Route::get('/my-rentals', [CustomerController::class, 'myRentals'])->name('my-rentals');
 });
